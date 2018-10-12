b3e895bfc0cc766214f65cd4003fdd42cbc4c877
Steven Muliamin <steven.muliamin@gmail.com>
2018-10-12 11:18:42 +0700

    added coredata files

---
+++ Bantu/CoreData/Needs+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Needs+CoreDataClass.swift
+//  
+//
+//  Created by Steven Muliamin on 12/10/18.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Needs)
+public class Needs: NSManagedObject {
+
+}
